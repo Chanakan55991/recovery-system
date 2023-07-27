147d24a0cbf101840713ef578d588f8e86e83941
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2023-07-27 11:36:01 +0700

    added login page

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+	import '@skeletonlabs/skeleton/themes/theme-modern.css'
+	import '@skeletonlabs/skeleton/styles/skeleton.css';
+
+	import '../app.postcss';
+</script>
+
+<slot />
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,34 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="h-screen bg flex-col flex justify-center items-center">
+	<div class="bg-white backdrop-blur-md box bg-opacity-50 p-4 flex flex-col justify-center items-center h-full w-[60%]">
+		<span class="mb-8 text-xl">สมัครสมาชิก</span>
+		<div class="flex flex-col">
+			<div class="flex flex-row items-center gap-8">
+				<div class="flex flex-col p-2 gap-y-2 h-full justify-between">
+					<div class="h-full flex items-center">
+						<span>รหัสประจำตัวนักเรียน</span>
+					</div>
+					<div class="h-full flex items-center">
+						<span>ชื่อ - นามสกุล</span>
+					</div>
+					<div class="h-full flex items-center">
+						<span>สาขา</span>
+					</div>
+					<div class="h-full flex items-center">
+						<span>รหัสผ่าน</span>
+					</div>
+					<div class="h-full flex items-center">
+						<span>ยืนยันรหัสผ่าน</span>
+					</div>
+				</div>
+				<div class="flex flex-col p-2 gap-y-2 w-[18rem]">
+					<input class="input p-2" type="text" placeholder="รหัสประจำตัว" />
+					<input class="input p-2" type="text" placeholder="ชื่อ-นามสกุล" />
+					<input class="input p-2" type="text" placeholder="สาขา" />
+					<input class="input p-2" type="text" placeholder="รหัสผ่าน" />
+					<input class="input p-2" type="text" placeholder="ยืนรหัสผ่าน" />
+				</div>
+			</div>
+			<button class="flex self-end btn variant-filled mt-4">Register</button>
+		</div>
+	</div>
+</div>
